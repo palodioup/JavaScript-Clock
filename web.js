4975e9cd41519ab13a8bc009026cf87a5eb5c3b1
Oladipupo Nathan Akanji <dipoakanji57@gmail.com>
2024-08-27 15:20:59 +0100

    javascript commit

---
+++ web.js
@@ -0,0 +1,11 @@
+function updateClock() {
+  const clockElement = document.getElementById("time");
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, "0");
+  const minutes = now.getMinutes().toString().padStart(2, "0");
+  const seconds = now.getSeconds().toString().padStart(2, "0");
+  clockElement.textContent = `${hours}:${minutes}:${seconds}`;
+}
+
+updateClock();
+setInterval(updateClock, 1000);
